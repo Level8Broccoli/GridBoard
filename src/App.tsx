86c5485c7562522d47fd38bae02127b97e3c2786
Oliver Bucher <oliver.bucher@getabstract.com>
2023-05-10 21:45:35 +0000

    add board state

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,43 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { Cell } from "./Cell"
 
-function App() {
-  const [height, setHeight] = useState(4)
-  const [width, setWidth] = useState(8)
+type BoardState = {
+  [coord: string]: boolean
+}
 
+function App() {
+  const [height, setHeight] = useState(1)
+  const [width, setWidth] = useState(2)
+  const [state, setState] = useState<BoardState>({})
+
+  useEffect(() => {
+    const newState: BoardState = {}
+    for (let i = 1; i <= height; i++) {
+      for (let j = 1; j <= width; j++) {
+        const coord = `${i}-${j}`;
+        const existing: boolean | undefined = state[coord]
+        if (existing === undefined) {
+          newState[coord] = false
+        } else {
+          newState[coord] = existing
+        }
+      }
+    }
+    console.log(newState, "useEffect");
+
+    setState(newState)
+
+  }, [width, height])
+
+  const toggler = (coord: string) => (to: boolean) => {
+    setState(prev => {
+      const newState = { ...prev }
+      newState[coord] = to;
+      console.log(newState, "toggler");
+
+      return newState
+    })
+  }
 
   return (
     <>
@@ -12,26 +45,33 @@ function App() {
         <div className="x-grid" style={{
           "--columns": `${width}`
         } as any}>
-          {[...Array(height * width).keys()].map((i) => {
-            return <div key={i}><Cell /></div>
+          {[...Array(height).keys()].map((i) => {
+            {
+              return [...Array(width).keys()].map((j) => {
+                const coord = `${i + 1}-${j + 1}`;
+                return <div key={coord}>
+                  <Cell isActive={state[coord]} toggleTo={toggler(coord)} coord={coord} />
+                </div>
+              })
+            }
           })}
         </div>
         <div className="right">
           <button className="secondary" onClick={
-            () => setWidth(Math.max(width + 1, 0))
+            () => setWidth(Math.max(width + 1, 1))
           }>Add</button>
           {width}
           <button className="secondary" onClick={
-            () => setWidth(Math.max(width - 1, 0))
+            () => setWidth(Math.max(width - 1, 1))
           }>Remove</button>
         </div>
         <div className="bottom">
           <button className="secondary" onClick={
-            () => setHeight(Math.max(height + 1, 0))
+            () => setHeight(Math.max(height + 1, 1))
           }>Add</button>
           {height}
           <button className="secondary" onClick={
-            () => setHeight(Math.max(height - 1, 0))
+            () => setHeight(Math.max(height - 1, 1))
           }>Remove</button>
         </div>
       </main>
--- src/Cell.tsx
+++ src/Cell.tsx
@@ -1,6 +1,5 @@
-import { useState } from "react"
-
-export function Cell() {
-    const [active, setActive] = useState(false)
-    return <button className={active ? "contrast" : ""} onClick={() => setActive(!active)}></button>
+export function Cell(props: { isActive: boolean, toggleTo: (to: boolean) => void, coord: string }) {
+    return <button className={props.isActive ? "contrast" : ""} onClick={() => props.toggleTo(!props.isActive)}>
+        {props.coord}
+    </button>
 }
